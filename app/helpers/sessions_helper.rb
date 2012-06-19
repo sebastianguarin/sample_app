6f59a3d2c5de74ce6cf2f4453bcefae695a19f53
Sebastian Guarin <sebastian@hlmbusiness.com>
2012-06-19 14:35:16 -0500

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,13 @@
 module SessionsHelper
 
-def sign_in(user)
+  def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
   end
 
-def signed_in?
+  def signed_in?
     !current_user.nil?
-end
+  end
 
   def current_user=(user)
     @current_user = user
@@ -16,8 +16,22 @@ end
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
-end
-def sign_out
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
   end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,13 +1,33 @@
 include ApplicationHelper
 
-def valid_signin(user)
+def sign_in(user)
+  visit signin_path
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
+  cookies[:remember_token] = user.remember_token
 end
 
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
-    page.should have_selector('div.alert.alert-error', text: message)
+  	page.should have_selector('div.alert.alert-error', text: message)
   end
+end
+
+RSpec::Matchers.define :have_correct_title  do |h1_text, title_text|
+  match do |page|
+  	if title_text.nil?
+    title_text = h1_text
+  	end
+  	page.should have_selector('h1',    text: h1_text) 
+    page.should have_selector('title', text: title_text) 
+  end
+end
+
+
+def valid_register
+	  fill_in "Name",         with: "Example User"
+    fill_in "Email",        with: "user@example.com"
+    fill_in "Password",     with: "foobar"
+    fill_in "Confirmation", with: "foobar"
 end
